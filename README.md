2061041b4b4b23e6e7a98d85e2fe137ef827b788
arkawa <arturkawa111@gmail.com>
2022-10-09 14:55:21 +0200

    Komentarz i zdjęcie

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Podyplomowe
 Studia Podyplomowe - laboratoria Git
+[Java] (https://imagazine.pl/wp-content/uploads/2014/12/javaprob-001.png)
--- src/lab1.java
+++ src/lab1.java
@@ -1,6 +1,6 @@
 public class lab1 {
     public static void main(String[] args) {
-
+// nazwa uzytkownika
         System.out.println("Nazywam się Artur");
     }
 }
